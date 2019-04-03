03ece8268ac9b54a26914a58c4d3ec0fb4744628
Phil E. Taylor <phil@phil-taylor.com>
2019-04-03 16:04:44 +0100

    add rules and cleanup

--- src/CleanupPlugin.php
+++ src/CleanupPlugin.php
@@ -3,9 +3,12 @@
 namespace Barryvdh\Composer;
 
 use Composer\Composer;
+use Composer\Config;
 use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Repository\WritableRepositoryInterface;
+use Composer\Script\Event;
 use Composer\Script\ScriptEvents;
 use Composer\Installer\PackageEvent;
 use Composer\Script\CommandEvent;
@@ -14,52 +17,53 @@ use Composer\Package\BasePackage;
 
 class CleanupPlugin implements PluginInterface, EventSubscriberInterface
 {
-    /** @var  \Composer\Composer $composer */
+    /** @var Composer $composer */
     protected $composer;
-    /** @var  \Composer\IO\IOInterface $io */
+    /** @var IOInterface $io */
     protected $io;
-    /** @var  \Composer\Config $config */
+    /** @var Config $config */
     protected $config;
-    /** @var  \Composer\Util\Filesystem $filesystem */
+    /** @var Filesystem $filesystem */
     protected $filesystem;
-    /** @var  array $rules */
+    /** @var array $rules */
     protected $rules;
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public function activate(Composer $composer, IOInterface $io)
     {
-        $this->composer = $composer;
-        $this->io = $io;
-        $this->config = $composer->getConfig();
+        $this->composer   = $composer;
+        $this->io         = $io;
+        $this->config     = $composer->getConfig();
         $this->filesystem = new Filesystem();
-        $this->rules = CleanupRules::getRules();
+        $this->rules      = CleanupRules::getRules();
     }
 
     /**
-     * {@inheritDoc}
+     * {@inheritdoc}
      */
     public static function getSubscribedEvents()
     {
-        return array(
-            ScriptEvents::POST_PACKAGE_INSTALL  => array(
-                array('onPostPackageInstall', 0)
-            ),
-            ScriptEvents::POST_PACKAGE_UPDATE  => array(
-                array('onPostPackageUpdate', 0)
-            ),
-            ScriptEvents::POST_INSTALL_CMD  => array(
-                array('onPostInstallUpdateCmd', 0)
-            ),
-            ScriptEvents::POST_UPDATE_CMD  => array(
-                array('onPostInstallUpdateCmd', 0)
-            ),
-        );
+        return [
+            ScriptEvents::POST_PACKAGE_INSTALL  => [
+                ['onPostPackageInstall', 0],
+            ],
+            ScriptEvents::POST_PACKAGE_UPDATE  => [
+                ['onPostPackageUpdate', 0],
+            ],
+            ScriptEvents::POST_INSTALL_CMD  => [
+                ['onPostInstallUpdateCmd', 0],
+            ],
+            ScriptEvents::POST_UPDATE_CMD  => [
+                ['onPostInstallUpdateCmd', 0],
+            ],
+        ];
     }
 
     /**
-     * Function to run after a package has been installed
+     * Function to run after a package has been installed.
+     * @param PackageEvent $event
      */
     public function onPostPackageInstall(PackageEvent $event)
     {
@@ -70,7 +74,8 @@ class CleanupPlugin implements PluginInterface, EventSubscriberInterface
     }
 
     /**
-     * Function to run after a package has been updated
+     * Function to run after a package has been updated.
+     * @param PackageEvent $event
      */
     public function onPostPackageUpdate(PackageEvent $event)
     {
@@ -81,17 +86,17 @@ class CleanupPlugin implements PluginInterface, EventSubscriberInterface
     }
 
     /**
-     * Function to run after a package has been updated
+     * Function to run after a package has been updated.
      *
      * @param CommandEvent $event
      */
-    public function onPostInstallUpdateCmd(\Composer\Script\Event $event)
+    public function onPostInstallUpdateCmd(Event $event)
     {
-        /** @var \Composer\Repository\WritableRepositoryInterface $repository */
+        /** @var WritableRepositoryInterface $repository */
         $repository = $this->composer->getRepositoryManager()->getLocalRepository();
 
         /** @var \Composer\Package\CompletePackage $package */
-        foreach($repository->getPackages() as $package){
+        foreach ($repository->getPackages() as $package) {
             if ($package instanceof BasePackage) {
                 $this->cleanPackage($package);
             }
@@ -101,43 +106,44 @@ class CleanupPlugin implements PluginInterface, EventSubscriberInterface
     /**
      * Clean a package, based on its rules.
      *
-     * @param BasePackage  $package  The package to clean
+     * @param BasePackage $package The package to clean
+     *
      * @return bool True if cleaned
      */
     protected function cleanPackage(BasePackage $package)
     {
         // Only clean 'dist' packages
-        if ($package->getInstallationSource() !== 'dist') {
+        if ('dist' !== $package->getInstallationSource()) {
             return false;
         }
 
-        $vendorDir = $this->config->get('vendor-dir');
-        $targetDir = $package->getTargetDir();
+        $vendorDir   = $this->config->get('vendor-dir');
+        $targetDir   = $package->getTargetDir();
         $packageName = $package->getPrettyName();
-        $packageDir = $targetDir ? $packageName . '/' . $targetDir : $packageName ;
+        $packageDir  = $targetDir ? $packageName.'/'.$targetDir : $packageName;
 
         $rules = isset($this->rules[$packageName]) ? $this->rules[$packageName] : null;
-        if(!$rules){
+        if (!$rules) {
             return;
         }
 
-        $dir = $this->filesystem->normalizePath(realpath($vendorDir . '/' . $packageDir));
+        $dir = $this->filesystem->normalizePath(realpath($vendorDir.'/'.$packageDir));
         if (!is_dir($dir)) {
             return false;
         }
 
-        foreach((array) $rules as $part) {
+        foreach ((array) $rules as $part) {
             // Split patterns for single globs (should be max 260 chars)
             $patterns = explode(' ', trim($part));
-            
+
             foreach ($patterns as $pattern) {
                 try {
                     foreach (glob($dir.'/'.$pattern) as $file) {
                         $this->filesystem->remove($file);
-                        $this->io->write("Removing file: ".$file);
+                        $this->io->write('[CleanupPlugin] Removing file: '.$file);
                     }
                 } catch (\Exception $e) {
-                    $this->io->write("Could not parse $packageDir ($pattern): ".$e->getMessage());
+                    $this->io->write("[CleanupPlugin] Could not parse $packageDir ($pattern): ".$e->getMessage());
                 }
             }
         }
